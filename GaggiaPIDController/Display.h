508cc958d0b07e99547bcf8d49df2fa42b91361d
Alberto Cardellini <albe.carde@gmail.com>
2020-04-19 16:58:32 +0100

    Fixed display height

--- GaggiaPIDController/Display.h
+++ GaggiaPIDController/Display.h
@@ -10,10 +10,8 @@
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET -1    // Reset pin # (or -1 if sharing Arduino reset pin)
-#define LOGO_HEIGHT 16
-#define LOGO_WIDTH 16
 
 class Display
 {
